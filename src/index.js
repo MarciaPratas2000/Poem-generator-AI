6516b91189e9e0cc1ae4341dcb874514973591c9
MarciaPratas2000 <156352013+MarciaPratas2000@users.noreply.github.com>
2024-02-20 14:28:34 +0000

    added basic javascript and added typewriter pluginplugi

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("hello");
+function generatePoem(event) {
+    event.preventDefault();
+  
+    new Typewriter("#poem", {
+      strings: "Poem generating",
+      autoStart: true,
+      delay: 1,
+      cursor: "",
+    });
+  }
+  
+  let poemFormElement = document.querySelector("#poem-form");
+  poemFormElement.addEventListener("submit", generatePoem);
