4b31bca6cf85cd71b9ea307996372364c02766c1
MarciaPratas2000 <156352013+MarciaPratas2000@users.noreply.github.com>
2024-02-22 14:32:31 +0000

    Enhanced Context and Prompt Formatting

--- src/index.js
+++ src/index.js
@@ -29,16 +29,15 @@ function handlePoemResponse(response) {
 
 // Function to generate a poem
 function generatePoem() {
-
   // User input and API details
   let instructionsInput = document.querySelector("#poemInput");
   let apiKey = "ec0ft3ef184fa26o40bf0860bad82dc8";
   let context =
-    "You are a poetic guide inspired by the harmony of nature. Your mission is to craft a poem that revolves around the essence of personal growth and nature's wisdom, with a focus on the user's instructions as the centerpiece. Each line should resonate with the soothing beauty of the natural world. Sign the poem with 'NatureVerseCraft' at the end in a different paragraph. The poem should have a maximum of 20 words.";
-  let prompt = `Create a poem where the profound connection between personal growth and nature unfolds, with ${instructionsInput.value} at its very heart. Make sure to mention the exact word ${instructionsInput.value} in your poem.`;
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(prompt)}&context=${encodeURIComponent(context)}&key=${apiKey}`;
+    "You are a poetic guide inspired by the harmony of nature. Your mission is to craft a poem that revolves around the essence of personal growth and nature's wisdom, with a focus on the user's instructions and nature as the centerpiece.The poem should have a maximum of 20 words and 4 lines. Your poems are short and sweet.";
+    let prompt = `User instructions are: Create a poem with ${instructionsInput.value} at its very heart. Make sure to mention the exact word ${instructionsInput.value} in your poem. Please make sure to separate each line of the poem with a paragraph of html <p>. Sign the poem with <strong>NatureVerseCraft</strong>.`;
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  // Access the element with ID 'poem'
+// Access the element with ID 'poem'
 let poemElement = document.getElementById('poem')
 // Add a class to the element
 poemElement.classList.add('poem');
